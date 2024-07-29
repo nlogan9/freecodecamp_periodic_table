600cebbed584b383614f5eacf30e46d20fea2b15
Nathan Logan <nathanlogan81@gmail.com>
2024-07-29 20:31:26 +0000

    feat: added atomic number lookup

--- element.sh
+++ element.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c" 
+
 if [[ -z $1 ]]
 then
   echo -e "\nPlease provide an element as an argument"
 fi
 
+NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
+
+if [[ $NUMBER ]]
+then
+  ELEMENT=$($PSQL "SELECT name FROM elements WHERE atomic_number = $1")
+  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $1")
+  echo -e "\nThe element with atomic number $NUMBER is $ELEMENT ($SYMBOL)"
+else
+  echo "new number"
+fi
