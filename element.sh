64c2eb429d7892b46479b277764287b1867ea6a8
Nathan Logan <nathanlogan81@gmail.com>
2024-07-30 19:02:51 +0000

    fix: suppressed error messages from output

--- element.sh
+++ element.sh
@@ -5,19 +5,19 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 if [[ -z $1 ]]
 then
   echo -e "\nPlease provide an element as an argument"
-fi
-
-if [[ $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1") ]]
+  exit
+elif [[ $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1" 2> /dev/null) ]]
 then
   NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1") | sed -E 's/^ *| *$//g')
-elif [[ $($PSQL "SELECT symbol FROM elements WHERE symbol = '$1'") ]]
+elif [[ $($PSQL "SELECT symbol FROM elements WHERE symbol = '$1'" 2> /dev/null) ]]
 then
   NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1'") | sed -E 's/^ *| *$//g')
-elif [[ $($PSQL "SELECT name FROM elements WHERE name = '$1'") ]]
+elif [[ $($PSQL "SELECT name FROM elements WHERE name = '$1'" 2> /dev/null) ]]
 then
   NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE name = '$1'") | sed -E 's/^ *| *$//g')
 else
   echo "I could not find that element in the database."
+  exit
 fi
 
   ELEMENT=$(echo $($PSQL "SELECT name FROM elements WHERE atomic_number = $NUMBER") | sed -E 's/^ *| *$//g')
