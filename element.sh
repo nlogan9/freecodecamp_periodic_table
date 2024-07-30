a0141c8723d41eca3129a3613d836552f4546c41
Nathan Logan <nathanlogan81@gmail.com>
2024-07-30 19:06:50 +0000

    fix: removed extra lines from output

--- element.sh
+++ element.sh
@@ -4,7 +4,7 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 if [[ -z $1 ]]
 then
-  echo -e "\nPlease provide an element as an argument"
+  echo "Please provide an element as an argument."
   exit
 elif [[ $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1" 2> /dev/null) ]]
 then
@@ -26,5 +26,5 @@ fi
   MASS=$(echo $($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $NUMBER") | sed -E 's/^ *| *$//g')
   MELT=$(echo $($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $NUMBER") | sed -E 's/^ *| *$//g')
   BOIL=$(echo $($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $NUMBER") | sed -E 's/^ *| *$//g')
-  echo -e "\nThe element with atomic number $NUMBER is $ELEMENT ($SYMBOL). It's a $TYPE, with a mass of $MASS amu.\
+  echo "The element with atomic number $NUMBER is $ELEMENT ($SYMBOL). It's a $TYPE, with a mass of $MASS amu.\
  $ELEMENT has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
